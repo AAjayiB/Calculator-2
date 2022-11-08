465b0d9d35ab69720062f5dc008f110283946f02
toumangd <toumangd@tcd.ie>
2022-11-08 12:00:29 +0000

    added division functionality

--- calculator.py
+++ calculator.py
@@ -7,16 +7,33 @@ def calculate(exp):
 
     if exp:
         # checks for any non operators or digits
-        if not re.search(r"[^\d\+\-\*]",exp):
+        if not re.search(r"[^\d\+\-\*\/\^]",exp):
             # checks for expressions beginning only with a single minus or not
             if re.search(r"^\-?\d",exp):
                 # checks for expressions ending with only digits 
                 if re.search(r"\d$", exp):
                     ############# ACTUAL OPERATIONS #############
-                    validExpression = re.findall(r"\d+|[\+\-\*]", exp)
+                    validExpression = re.findall(r"\d+|[\+\-\*\/\^]", exp)
                     ############# Here at the moment
                     if validExpression[0] =="-":
                         validExpression[0:2]=["".join(validExpression[0:2])]
+
+                    while (validExpression.count("/") > 0):
+                        opIndex = validExpression.index("/")
+                        if validExpression[opIndex-1]=="+" or validExpression[opIndex-1]=="-" or validExpression[opIndex-1]=="/": 
+	                        return CONSECUTIVE_OPERATORS
+                        if (validExpression[opIndex+1]=="+" or validExpression[opIndex+1]=="*" or validExpression[opIndex+1]=="/"):
+                            return CONSECUTIVE_OPERATORS
+                        if (validExpression[opIndex+1]=="+" or validExpression[opIndex+1]=="*") and (validExpression[opIndex+2]=="+" or validExpression[opIndex+2]=="-" or validExpression[opIndex+2]=="*" or validExpression[opIndex+2]=="/"): 
+	                        return CONSECUTIVE_OPERATORS
+                        if validExpression[opIndex+1] =="-" and (validExpression[opIndex+2]=="+" or validExpression[opIndex+2]=="-" or validExpression[opIndex+2]=="*" or validExpression[opIndex+2]=="/"): 
+	                        return CONSECUTIVE_OPERATORS
+                        if validExpression[opIndex+1] =="-":
+                            validExpression[opIndex+1:opIndex+3] = ["".join(validExpression[opIndex+1:opIndex+3])]
+                        computedValue = int(validExpression[opIndex-1])/int(validExpression[opIndex+1])
+                        validExpression[opIndex-1] = computedValue
+                        validExpression.pop(opIndex+1)
+                        validExpression.pop(opIndex)
 
                     while (validExpression.count("*") > 0):
                         opIndex = validExpression.index("*")
