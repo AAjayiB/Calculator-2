f9854143543d54bbe3dd07bd12634af91d7ec656
Aranq13 <98594133+Aranq13@users.noreply.github.com>
2022-11-08 23:29:21 +0000

    Update calculatorTests.py
    Fix

--- calculatorTests.py
+++ calculatorTests.py
@@ -3,15 +3,25 @@ NO_INPUT_ERROR = "No input was entered."
 INVALID_INPUT_ERROR = "Not a valid input"
 MISPLACED_OPERATORS = "Misplaced operators"
 CONSECUTIVE_OPERATORS_ERROR = "Invalid string, contains consecutive operators"
+DIVIDE_BY_ZERO_ERROR = "Cannot divide by zero"
 def testCalculate():
     # Test for no string input
     assert calculate("")==NO_INPUT_ERROR
 
     # Tests for basic computations
-
     assert calculate("1+2+3+4")==10
     assert calculate("1*2*3*4")==24
     assert calculate("6-5-4-3")==-6
+
+    
+        
+          
+    
+
+        
+    
+    @@ -29,6 +29,7 @@ def testCalculate():
+  
     assert calculate("25/5")==5.0
     assert calculate("30/12")==2.5
     assert calculate("4^2")==16
@@ -19,7 +29,6 @@ def testCalculate():
     assert calculate("log(25)")==3.219
     assert calculate("exp(2+2)")==54.598
     assert calculate("log(5^2)")==3.219
-
     # Tests for precedence of combined operations
     assert calculate("6*2-7+4")==9
     assert calculate("8+1*1-9")==0
@@ -29,17 +38,29 @@ def testCalculate():
     assert calculate("exp(2+2)")==54.598
     assert calculate("log(5^2)")==3.219
 
-
     # Tests for invalid numbers
     assert calculate("x+1")== INVALID_INPUT_ERROR
     assert calculate("p-1")== INVALID_INPUT_ERROR
-    assert calculate("a*1")== INVALID_INPUT_ERROR
 
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+          
+    
+    @@ -69,10 +70,12 @@ def testCalculate():
+  
+    assert calculate("a*1")== INVALID_INPUT_ERROR
     # Tests for invalid operations
     assert calculate("1x2")==INVALID_INPUT_ERROR
     assert calculate("1=2")==INVALID_INPUT_ERROR
     assert calculate("log12")==INVALID_INPUT_ERROR
-
     # Tests for strings beginning and ending with an operation
     assert calculate("1+2+3+")==MISPLACED_OPERATORS
     assert calculate("1+2+3*")==MISPLACED_OPERATORS
@@ -60,7 +81,6 @@ def testCalculate():
     assert calculate("2*-2")==-4
     assert calculate("-2*3")==-6
     assert calculate("3--2")==5
-
     # Tests for consecutive operators
     assert calculate("2+++2")==CONSECUTIVE_OPERATORS_ERROR
     assert calculate("2---2")==CONSECUTIVE_OPERATORS_ERROR
@@ -71,11 +91,9 @@ def testCalculate():
     assert calculate("2*+2")==CONSECUTIVE_OPERATORS_ERROR
 
 
-    
     # Test for brackets 
     assert calculate("100/(25-8)")==5.882
     assert calculate("99*2^(exp(3)/log(15))")==16918.690
     # Tests for divison by 0
     assert calculate("5/0")==DIVIDE_BY_ZERO_ERROR
     assert calculate("12/(10-10)")==DIVIDE_BY_ZERO_ERROR
-
