7f5a055906ad0ec88cf9d8cc1b0b4b3f8ef6770b
Ameen <72974478+AAjayiB@users.noreply.github.com>
2022-11-08 22:44:30 +0000

    Update calculatorTests.py

--- calculatorTests.py
+++ calculatorTests.py
@@ -16,10 +16,7 @@ def testCalculate():
     assert calculate("25/5")=="5.0"
     assert calculate("30/12")=="2.5"
     assert calculate("4^2")=="16"
-    assert calculate("exp(4)")=="54.598"
-    assert calculate("log(25)")=="3.219"
-    assert calculate("exp(2+2)")=="54.598"
-    assert calculate("log(5^2)")=="3.219"
+
 
     # Tests for precedence of combined operations
     assert calculate("6*2-7+4")=="9"
@@ -27,8 +24,7 @@ def testCalculate():
     assert calculate("99-24+9*4")=="111"
     assert calculate("1+2-4/2")=="1"
     assert calculate("2^2-4/2")=="2"
-    assert calculate("exp(2+2)")=="54.598"
-    assert calculate("log(5^2)")=="3.219"
+   
 
     # Tests for invalid numbers
     assert calculate("x+1")== INVALID_INPUT_ERROR
@@ -70,10 +66,6 @@ def testCalculate():
     assert calculate("2-+2")==CONSECUTIVE_OPERATORS_ERROR
     assert calculate("2*+2")==CONSECUTIVE_OPERATORS_ERROR
 
-    
-    # Test for brackets 
-    assert calculate("100/(25-8)")=="5.882"
-    assert calculate("99*2^(exp(3)/log(15))")=="16918.960"
     # Tests for divison by 0
     assert calculate("5/0")==DIVIDE_BY_ZERO_ERROR
     assert calculate("12/(10-10)")==DIVIDE_BY_ZERO_ERROR
