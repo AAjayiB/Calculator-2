c65c3036af4157cbaebf4ef654c40ef9ea5112b6
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-11-08 13:25:43 +0000

    Changed output

--- calculator.py
+++ calculator.py
@@ -8,16 +8,19 @@ l = "log"
 
 def evaluate(exp):
     answer = (0, "")
-
+    
     if exp:
         if re.search(r"[$&]", exp):
-            return -1, ERROR_MESSAGES[1]
+            return ERROR_MESSAGES[1]
         exp = exp.replace("log(", "$(")
         exp = exp.replace("exp(", "&(")
         answer = calculate(exp)
     else:
         answer = (-1, ERROR_MESSAGES[0])
-    return answer
+    if answer[1]=="":
+        return round(float(answer[0]),3)
+    else:
+        return answer[1]
 
 
 def calculate(exp):
@@ -139,10 +142,7 @@ def Main():
     while (True):
         print("Enter an expression")
         result = evaluate(input())
-        if result[1] == "":
-            print(result[0])
-        else:
-            print(result[1])
+        print(result)
     # r=re.search(r"log","shsudflogfjswi")
     # if r():
     #     print("is there")
